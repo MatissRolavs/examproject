8f501aa1e0f3f6744dc13fab7c65ee0bf7c71224
Matiss Rolavs <matissrolavs2005@gmail.com>
2024-05-10 08:12:20 +0300

    finish 3.0

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -5,7 +5,7 @@
     </div>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 ">
-            <div class="bg-yellow-200 dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg" >
+            <div class="bg-yellow-200 overflow-hidden shadow-sm sm:rounded-lg" >
                 <div class="p-6 text-gray-900 dark:text-gray-100 flex flex-wrap">
                     @if ($userType == "admin")
                     <a href="/games-create" class="text-blue-500 hover:underline ">Create New Game</a>
